87385bb38302bbc5807cb4454b47a5e5a95bc18e
lionel <musoke.lionel@gmail.com>
2025-06-16 10:01:45 +0200

    Python Coursework

---
+++ Python_coursework.py
@@ -0,0 +1,8 @@
+Roll_number=(input("Enter Roll Number:"))
+Grade=(input("Enter Grade:"))
+Name=(input("Enter Student Name:"))
+
+
+print("Student Name",Name)
+print("Student Roll Number:",Roll_number ,sep=":")
+print("Student Grade" ,Grade)
