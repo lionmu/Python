a3a03eca8b5eac68f1cd65a3d64e2f15000776c3
Lionel <musoke.lionel@gmail.com>
2025-06-17 03:55:26 +0200

    String_functions

---
+++ string_functions_or_methods.py
@@ -0,0 +1,16 @@
+#str.capitslize() makes the start of the string output as upper case and the rest lower case
+
+
+#syntax str.capitalize()
+str1='PYTHON'
+print(str1.capitalize())
+#output will be Python
+
+#str.find() and str.index() Will return the output as the index value of the character
+
+#Example using the str1
+#str1.find()
+print(str1.find("z"))
+#using str1.index()
+print(str1.index('N'))
+
