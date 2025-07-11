d6e3e56af4eeaacb10aeb9c4f02dea7df4df0547
Lionel <musoke.lionel@gmail.com>
2025-07-11 16:03:33 +0200

    Continuation of Operators

--- Operators.py
+++ Operators.py
@@ -23,3 +23,20 @@
 
 #list1=[0]*10
 #print(list1)
+
+#logical Operators used to combine statements or boolean expressions
+#THese Logical And Logical Or and Logical Not
+
+#Loical And :Both expressions have to be true for the result to be true else false.
+#Example
+a,b= 5,6
+c=True
+print(a>4 and b<10) #Both conditions are true so it will print TRUE as output
+
+#Logical Or :Either of the expressions has to be true  so as for the condition to be true but of both conditions are  false
+#then Expression will be false.
+#Example
+print (a>4 or b<2)
+#logical Not : Will give the opposite of the output. if true it will return false and the reverse is true
+#Example
+print(not c )
