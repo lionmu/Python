d46dc02a2b6afd58432c247d5554ac4e56e657b8
lionel <musoke.lionel@gmail.com>
2025-07-16 16:48:09 +0200

    Roller Coaster Completed

--- Rolller Coaster Program.py
+++ Rolller Coaster Program.py
@@ -17,7 +17,7 @@ if height>=3:
         if photo_input=='yes':
             billed_amount=photo_bill+age_under_12
             print(f'{billed_amount}rs WILL BE YOUR TOTAL BILL')
-        elif photo_input=='no':
+        elif photo_input=='no':                                     #when you use elif means you are having more than one condition YES AND NO
            print('PLEASE PROCEED WITH PAYMENT AND RIDE')
 
     elif age<=18:
@@ -26,7 +26,9 @@ if height>=3:
         if photo_input=='yes':
             billed_amount=age_between_12_to_18+photo_bill
             print(f'{billed_amount}rs WILL BE YOUR TOTAL BILL')
-        else:
+        else:#when using else means you want to use only one condition but i think by default it will cater for the other condition in this case its NO
+            
+
             print('PLEASE PROCEED WITH YOUR RIDE')
     else:
         print('PAY 500Rs')
