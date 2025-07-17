30af8029a93475548510d94a000c9651f73168c4
Lionel <musoke.lionel@gmail.com>
2025-07-17 10:52:24 +0200

    Roller Coaster Program

--- Rolller Coaster Program.py
+++ Rolller Coaster Program.py
@@ -17,22 +17,27 @@ if height>=3:
         if photo_input=='yes':
             billed_amount=photo_bill+age_under_12
             print(f'{billed_amount}rs WILL BE YOUR TOTAL BILL')
-        elif photo_input=='no':
+        else:
             print('PLEASE PROCEED AND ENJOY YOUR RIDE')
 
-        elif age <=18:
-         print('PAY 250 RUPEES')
+    elif age <=18:
+        print('PAY 250 RUPEES')
         photo_input=input('DO YOU WANT TO TAKE A PHOTO:').lower()
-        billed_amount=photo_bill+age_between_12_to_18
-        print(f'{billed_amount}rs WILL BE YOUR BILL')
-
+        if photo_input=='yes':
+            billed_amount=photo_bill+age_18_and_above
+            print(f'{billed_amount}rs WILL BE YOUR TOTAL BILL')
+        else:
+              print('PLEASE PROCEED AND ENJOY THE RIDE')
 
     else:
-     print('PAY 500 RUPEES')
-     photo_input=input('DO YOU WANT TO TAKE A PHOTO:').lower()
-    billed_amount=photo_bill+age_18_and_above
-    print(f'{billed_amount}rs WILL BE YOUR BILL')
+           print('PAY 500 RUPEES')
+           photo_input=input('DO YOU WANT TO TAKE A PHOTO:').lower()
+           if photo_input=='yes':
+              billed_amount = photo_bill + age_18_and_above
+              print (f'{billed_amount} WILL BE YOUR TOTAL FARE')
+           else:
+              print('PLEASE PROCEED AND ENJOY THE RIDE')
 
 else:
  print('YOU ARE NOT QUALIFIED TO RIDE PLEASE COME AGAIN')
-print('BYE AND COME AGAIN NEXT TIME')
+ print('BYE AND COME AGAIN NEXT TIME')
