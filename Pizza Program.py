fe9ea65d77a8b1ea15f515ee32bea96523bdd376
Lionel <musoke.lionel@gmail.com>
2025-07-24 10:53:31 +0200

    FINAL PIZZA HUT APP

--- Pizza Program.py
+++ Pizza Program.py
@@ -4,33 +4,34 @@ print('OPTION 1 SMALL SIZE PIZZA')
 print('OPTION 2 MEDIUM SIZE PIZZA')
 print('OPTION 3 LARGE SIZE PIZZA')
 option = int(input('CHOOSE OPTION:'))
-intial_cost=0
+
+initial_cost=0
 
 if option == 1:
-    intial_cost+=100
-    print(f'YOUR PIZZA WILL COST {intial_cost}Rs')
+    initial_cost+=100
+    print(f'YOUR PIZZA WILL COST {initial_cost}Rs')
     
 elif option==2:
-    intial_cost+=200
-    print(f'PIZZA WILL COST{intial_cost}Rs')
-    
-else:
-    intial_cost+=300
-    print(f'PIZZA WILL COST {intial_cost}Rs')
+    initial_cost+=200
+    print(f'PIZZA WILL COST{initial_cost}Rs')
 
-additional_pepperoni=input('DO YOU WANR EXTRA PEPPERONI:').lower
-if additional_pepperoni=='yes':
-     if option==1:
-      intial_cost+=30
-    
 else:
-      intial_cost+=50
+    initial_cost+=300
+    print(f'PIZZA WILL COST {initial_cost}Rs')
+
+additional_pepperoni=input('EXTRA PEPPERONI (Y/N):')
+if additional_pepperoni.lower()=='y':
+  if option==1:
+   initial_cost+=30
+    
+  else:
+      initial_cost+=50
          
-additional_cheese=input('DO YOU REQUIRE EXTRA CHEESE:').lower
-if additional_cheese=='Yes':
-     intial_cost+=20
+additional_cheese=input('EXTRA CHEESE (Y/N):')
+if additional_cheese.lower()=='y':
+   initial_cost+=20
           
-print(f'YOUR TOTAL BILL WILL BE {intial_cost}Rs')  
+print(f'YOUR TOTAL BILL WILL BE {initial_cost}Rs')
 print('THANK YOU')
 
 
